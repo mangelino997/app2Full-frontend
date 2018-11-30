ae7a39ed7170275cf6e70d6d3327c7f32e05d089
Marcio Angelino <marcio@draimo.com>
2018-11-30 09:25:52 -0300

    Desarrollo completo del Html Moneda

--- src/app/componentes/moneda/moneda.component.ts
+++ src/app/componentes/moneda/moneda.component.ts
@@ -45,7 +45,7 @@ export class MonedaComponent implements OnInit {
   //Constructor
   constructor(private subopcionPestaniaService: SubopcionPestaniaService, private toastr: ToastrService,) {
     //Obtiene la lista de pestanias
-    this.subopcionPestaniaService.listarPorRolSubopcion(1, 195)
+    this.subopcionPestaniaService.listarPorRolSubopcion(1, 19)
     .subscribe(
       res => {
         this.pestanias = res.json();
@@ -56,13 +56,130 @@ export class MonedaComponent implements OnInit {
         console.log(err);
       }
     );
-    /*Se subscribe al servicio de lista de registros
-    this.servicio.listaCompleta.subscribe(res => {
-      this.listaCompleta = res;
-    });*/
+    
    }
 
   ngOnInit() {
+    //Define el formulario y validaciones
+    this.formulario = new FormGroup({
+    id: new FormControl(),
+    version: new FormControl(),
+    codigo: new FormControl('', ),
+    nombre: new FormControl('', [Validators.required, Validators.maxLength(45)]),
+    estaActivo: new FormControl('', [Validators.required, Validators.maxLength(45)])
+    });
+
+    //Establece los valores de la primera pestania activa
+    this.seleccionarPestania(1, 'Agregar', 0);
+    //El campo codigo del formulario siempre se mantiene deshabilitado
+    this.formulario.get('codigo').disable();
+  }
+
+  //Funcion para establecer los valores de las pestañas
+  private establecerValoresPestania(nombrePestania, autocompletado, soloLectura, boton, componente) {
+    this.pestaniaActual = nombrePestania;
+    this.mostrarAutocompletado = autocompletado;
+    this.soloLectura = soloLectura;
+    this.mostrarBoton = boton;
+    setTimeout(function () {
+      document.getElementById(componente).focus();
+    }, 20);
+  };
+  //Establece valores al seleccionar una pestania
+  public seleccionarPestania(id, nombre, opcion) {
+    this.formulario.reset();
+    this.indiceSeleccionado = id;
+    this.activeLink = nombre;
+    /*
+    * Se vacia el formulario solo cuando se cambia de pestania, no cuando
+    * cuando se hace click en ver o mod de la pestania lista
+    */
+    if(opcion == 0) {
+      this.autocompletado.setValue(undefined);
+      this.resultados = [];
+    }
+    switch (id) {
+      case 1:
+        // this.obtenerSiguienteId();
+        this.establecerEstadoCampos(true);
+        this.establecerValoresPestania(nombre, false, false, true, 'idNombre');
+        break;
+      case 2:
+        this.establecerEstadoCampos(false);
+        this.establecerValoresPestania(nombre, true, true, false, 'idAutocompletado');
+        break;
+      case 3:
+        this.establecerEstadoCampos(true);
+        this.establecerValoresPestania(nombre, true, false, true, 'idAutocompletado');
+        break;
+      case 4:
+        this.establecerEstadoCampos(false);
+        this.establecerValoresPestania(nombre, true, true, true, 'idAutocompletado');
+        break;
+      default:
+        break;
+    }
+  }
+  //Habilita o deshabilita los campos dependiendo de la pestaña
+  private establecerEstadoCampos(estado) {
+    if(estado) {
+      this.formulario.get('nombre').enable();
+      this.formulario.get('estaActivo').enable();
+    } else {
+      this.formulario.get('nombre').disable();
+      this.formulario.get('estaActivo').disable();
+    }
+  }
+  //Funcion para determina que accion se requiere (Agregar, Actualizar, Eliminar)
+  // public accion(indice) {
+  //   switch (indice) {
+  //     case 1:
+  //       this.agregar();
+  //       break;
+  //     case 3:
+  //       this.actualizar();
+  //       break;
+  //     case 4:
+  //       this.eliminar();
+  //       break;
+  //     default:
+  //       break;
+  //   }
+  // }
+  //Reestablece los campos formularios
+  private reestablecerFormulario(id) {
+    this.formulario.reset();
+    this.formulario.get('id').setValue(id);
+    this.autocompletado.setValue(undefined);
+    this.resultados = [];
+  }
+  //Manejo de colores de campos y labels
+  public cambioCampo(id, label) {
+    document.getElementById(id).classList.remove('is-invalid');
+    document.getElementById(label).classList.remove('label-error');
+  };
+  //Muestra en la pestania buscar el elemento seleccionado de listar
+  public activarConsultar(elemento) {
+    this.seleccionarPestania(2, this.pestanias[1].nombre, 1);
+    this.autocompletado.setValue(elemento);
+    this.formulario.patchValue(elemento);
+  }
+  //Muestra en la pestania actualizar el elemento seleccionado de listar
+  public activarActualizar(elemento) {
+    this.seleccionarPestania(3, this.pestanias[2].nombre, 1);
+    this.autocompletado.setValue(elemento);
+    this.formulario.patchValue(elemento);
+  }
+  //Maneja los evento al presionar una tacla (para pestanias y opciones)
+  public manejarEvento(keycode) {
+    var indice = this.indiceSeleccionado;
+    if(keycode == 113) {
+      if(indice < this.pestanias.length) {
+        this.seleccionarPestania(indice+1, this.pestanias[indice].nombre, 0);
+      } else {
+        this.seleccionarPestania(1, this.pestanias[0].nombre, 0);
+      }
+    }
   }
 
 }
