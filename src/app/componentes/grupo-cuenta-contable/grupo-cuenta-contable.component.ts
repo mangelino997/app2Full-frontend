fc690c81ef0f78a1386528f7418d56a5fe296b67
Marcio Angelino <marcio@draimo.com>
2019-02-14 12:49:28 -0300

    Desarrollo de Grupo Cuenta Contable + Pruebas de funcionamiento

---
+++ src/app/componentes/grupo-cuenta-contable/grupo-cuenta-contable.component.ts
@@ -0,0 +1,279 @@
+import { Component, OnInit } from '@angular/core';
+import { SubopcionPestaniaService } from '../../servicios/subopcion-pestania.service';
+import { AppComponent } from '../../app.component';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { TipoCuentaContableService } from 'src/app/servicios/tipo-cuenta-contable.service';
+import { GrupoCuentaContableService } from 'src/app/servicios/grupo-cuenta-contable.service';
+import { GrupoCuentaContable } from 'src/app/modelos/grupo-cuenta-contable';
+@Component({
+  selector: 'app-grupo-cuenta-contable',
+  templateUrl: './grupo-cuenta-contable.component.html',
+  styleUrls: ['./grupo-cuenta-contable.component.css']
+})
+export class GrupoCuentaContableComponent implements OnInit {
+
+  //Define la pestania activa
+  public activeLink:any = null;
+  //Define el indice seleccionado de pestania
+  public indiceSeleccionado:number = null;
+  //Define la pestania actual seleccionada
+  public pestaniaActual:string = null;
+  //Define si mostrar el autocompletado
+  public mostrarAutocompletado:boolean = null;
+  //Define si el campo es de solo lectura
+  public soloLectura:boolean = false;
+  //Define si mostrar el boton
+  public mostrarBoton:boolean = null;
+  //Define la lista de pestanias
+  public pestanias=null;
+  //Define un formulario para validaciones de campos
+  public formulario:FormGroup;
+  //Define la lista completa de registros
+  public listaCompleta:Array<any> = [];
+  //Define la lista completa de registros para Tipos de Cuenta Contable
+  public tiposCuentaContable:Array<any> = [];
+  //Define el autocompletado para las busquedas
+  public autocompletado:FormControl = new FormControl();
+  //Define la lista de resultados del autocompletado
+  public resultados:Array<any> = [];
+  //Constructor
+  constructor(private servicio: GrupoCuentaContableService, private subopcionPestaniaService: SubopcionPestaniaService,
+    private grupoCuentaContable: GrupoCuentaContable, private tipoCuentaContableService: TipoCuentaContableService, private appComponent: AppComponent, private toastr: ToastrService) {
+    //Obtiene la lista de pestania por rol y subopcion
+    this.subopcionPestaniaService.listarPorRolSubopcion(this.appComponent.getRol(), this.appComponent.getSubopcion())
+    .subscribe(
+      res => {
+        this.pestanias = res.json();
+        this.activeLink = this.pestanias[0].nombre;
+      },
+      err => {
+      }
+    );
+    //Se subscribe al servicio de lista de registros
+    this.servicio.listaCompleta.subscribe(res => {
+      this.listaCompleta = res;
+    });
+    //Autocompletado - Buscar por nombre
+    this.autocompletado.valueChanges.subscribe(data => {
+      if(typeof data == 'string') {
+        this.servicio.listarPorNombre(data).subscribe(res => {
+          this.resultados = res;
+        })
+      }
+    })
+  }
+  //Al iniciarse el componente
+  ngOnInit() {
+
+    //Define los campos para validaciones
+    this.formulario = this.grupoCuentaContable.formulario;
+    //Establece los valores de la primera pestania activa
+    this.seleccionarPestania(1, 'Agregar', 0);
+    //Obtiene la lista completa de registros
+    this.listar();
+    //Listar tipos de Cuenta Contable (establece el campo de seleccion)
+    this.listarTiposCuentaContable();
+  }
+  //Funcion para establecer los valores de las pestañas
+  private establecerValoresPestania(nombrePestania, autocompletado, soloLectura, boton, componente) {
+    this.pestaniaActual = nombrePestania;
+    this.mostrarAutocompletado = autocompletado;
+    this.soloLectura = soloLectura;
+    this.mostrarBoton = boton;
+    setTimeout(function () {
+      document.getElementById(componente).focus();
+    }, 20);
+  };
+  //Establece valores al seleccionar una pestania
+  public seleccionarPestania(id, nombre, opcion) {
+    
+    this.indiceSeleccionado = id;
+    this.activeLink = nombre;
+    if(opcion == 0) {
+      this.autocompletado.setValue(undefined);
+      this.resultados = [];
+    }
+    switch (id) {
+      case 1:
+        this.obtenerSiguienteId();
+        this.establecerEstadoCampos(true);
+        this.establecerValoresPestania(nombre, false, false, true, 'idNombre');
+        break;
+      case 2:
+        this.establecerEstadoCampos(false);
+        this.establecerValoresPestania(nombre, true, true, false, 'idAutocompletado');
+        break;
+      case 3:
+        this.establecerEstadoCampos(true);
+        this.establecerValoresPestania(nombre, true, false, true, 'idAutocompletado');
+        break;
+      case 4:
+        this.establecerEstadoCampos(false);
+        this.establecerValoresPestania(nombre, true, true, true, 'idAutocompletado');
+        break;
+      default:
+        break;
+    }
+  }
+  //Habilita o deshabilita los campos dependiendo de la pestaña
+  private establecerEstadoCampos(estado) {
+    if(estado) {
+      this.formulario.get('tipoCuentaContable').enable();
+    } else {
+      this.formulario.get('tipoCuentaContable').disable();
+    }
+  }
+  //Funcion para determina que accion se requiere (Agregar, Actualizar, Eliminar)
+  public accion(indice) {
+    switch (indice) {
+      case 1:
+        this.agregar();
+        break;
+      case 3:
+        this.actualizar();
+        break;
+      case 4:
+        this.eliminar();
+        break;
+      default:
+        break;
+    }
+  }
+  //Obtiene el siguiente id
+  private obtenerSiguienteId() {
+    this.servicio.obtenerSiguienteId().subscribe(
+      res => {
+        this.formulario.get('id').setValue(res.json());
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
+  //Obtiene el listado de registros
+  private listar() {
+    this.servicio.listar().subscribe(
+      res => {
+        this.listaCompleta = res.json();
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
+  //Obtiene la lista de Tipos de Cuenta Contable
+  private listarTiposCuentaContable(){
+    this.tipoCuentaContableService.listar().subscribe(
+      res => {
+        this.tiposCuentaContable = res.json();
+      },
+      err => {
+        console.log(err);
+      }
+    );    
+  }
+  //Agrega un registro
+  private agregar() {
+    this.servicio.agregar(this.formulario.value).subscribe(
+      res => {
+        var respuesta = res.json();
+        if(respuesta.codigo == 201) {
+          this.reestablecerFormulario(respuesta.id);
+          setTimeout(function() {
+            document.getElementById('idNombre').focus();
+          }, 20);
+          this.toastr.success(respuesta.mensaje);
+        }
+      },
+      err => {
+        var respuesta = err.json();
+        if(respuesta.codigo == 11002) {
+          document.getElementById("labelNombre").classList.add('label-error');
+          document.getElementById("idNombre").classList.add('is-invalid');
+          document.getElementById("idNombre").focus();
+          this.toastr.error(respuesta.mensaje);
+        }
+      }
+    );
+  }
+  //Actualiza un registro
+  private actualizar() {
+    this.servicio.actualizar(this.formulario.value).subscribe(
+      res => {
+        var respuesta = res.json();
+        if(respuesta.codigo == 200) {
+          this.reestablecerFormulario('');
+          setTimeout(function() {
+            document.getElementById('idAutocompletado').focus();
+          }, 20);
+          this.toastr.success(respuesta.mensaje);
+        }
+      },
+      err => {
+        var respuesta = err.json();
+        if(respuesta.codigo == 11002) {
+          document.getElementById("labelNombre").classList.add('label-error');
+          document.getElementById("idNombre").classList.add('is-invalid');
+          document.getElementById("idNombre").focus();
+          this.toastr.error(respuesta.mensaje);
+        }
+      }
+    );
+  }
+  //Elimina un registro
+  private eliminar() {
+    console.log();
+  }
+  //Reestablece el formulario
+  private reestablecerFormulario(id) {
+    this.formulario.reset();
+    this.formulario.get('id').setValue(id);
+    this.autocompletado.setValue(undefined);
+    this.resultados = [];
+  }
+  //Manejo de colores de campos y labels
+  public cambioCampo(id, label) {
+    document.getElementById(id).classList.remove('is-invalid');
+    document.getElementById(label).classList.remove('label-error');
+  };
+  //Muestra en la pestania buscar el elemento seleccionado de listar
+  public activarConsultar(elemento) {
+    this.seleccionarPestania(2, this.pestanias[1].nombre, 1);
+    this.autocompletado.setValue(elemento);
+    this.formulario.setValue(elemento);
+  }
+  //Muestra en la pestania actualizar el elemento seleccionado de listar
+  public activarActualizar(elemento) {
+    this.seleccionarPestania(3, this.pestanias[2].nombre, 1);
+    this.autocompletado.setValue(elemento);
+    this.formulario.setValue(elemento);
+  }
+  //Define como se muestra los datos en el autcompletado
+  public displayFn(elemento) {
+    if(elemento != undefined) {
+      return elemento.nombre ? elemento.nombre : elemento;
+    } else {
+      return elemento;
+    }
+  }
+  //Funcion para comparar y mostrar elemento de campo select
+  public compareFn = this.compararFn.bind(this);
+  private compararFn(a, b) {
+    if(a != null && b != null) {
+      return a.id === b.id;
+    }
+  }
+  //Maneja los evento al presionar una tacla (para pestanias y opciones)
+  public manejarEvento(keycode) {
+    var indice = this.indiceSeleccionado;
+    if(keycode == 113) {
+      if(indice < this.pestanias.length) {
+        this.seleccionarPestania(indice+1, this.pestanias[indice].nombre, 0);
+      } else {
+        this.seleccionarPestania(1, this.pestanias[0].nombre, 0);
+      }
+    }
+  }
+}
+
