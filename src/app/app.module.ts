4e5b5e74766c0b70826541707fbd66099192bb7c
marcio <marcio@draimo.com>
2019-06-28 12:46:36 -0300

    Correcciones en Orden Venta + Pruebas de funcionamiento. Falta definir algunos campos en el formulario del dialogo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -249,7 +249,7 @@ import { VendedorComponent } from './componentes/vendedor/vendedor.component';
 import { ZonaComponent } from './componentes/zona/zona.component'; //Probado
 import { ClienteComponent } from './componentes/cliente/cliente.component'; //Probado
 import { ResumenClienteComponent } from './componentes/resumen-cliente/resumen-cliente.component';
-import { OrdenVentaComponent, VerTarifaDialogo, ModificarTarifaDialogo, EliminarTarifaDialogo } from './componentes/orden-venta/orden-venta.component';
+import { OrdenVentaComponent, VerTarifaDialogo } from './componentes/orden-venta/orden-venta.component';
 import { ProveedorComponent } from './componentes/proveedor/proveedor.component'; //Probado
 import { CondicionCompraComponent } from './componentes/condicion-compra/condicion-compra.component';
 import { PersonalComponent } from './componentes/personal/personal.component'; //Probado
@@ -448,7 +448,7 @@ const appRoutes: Routes = [
 ]
 
 const stompConfig: StompConfig = {
-  url: 'ws://192.168.0.156:8080/jitws/socket', // ws://localhost:8080/jitws/socket - ws://gestionws.appspot.com:8080/jitws/socket
+  url: 'ws://192.168.0.123:8080/jitws/socket', // ws://localhost:8080/jitws/socket - ws://gestionws.appspot.com:8080/jitws/socket
   headers: {},
   heartbeat_in: 0,
   heartbeat_out: 20000,
@@ -601,8 +601,7 @@ const stompConfig: StompConfig = {
     TalonarioReciboCobradorComponent,
     TalonarioReciboLoteComponent,
     VerTarifaDialogo,
-    ModificarTarifaDialogo,
-    EliminarTarifaDialogo,
+    
   ],
   imports: [
     BrowserModule,
@@ -885,8 +884,6 @@ const stompConfig: StompConfig = {
     PersonalFamiliarComponent,
     PlanCuentaDialogo,
     VerTarifaDialogo,
-    ModificarTarifaDialogo,
-    EliminarTarifaDialogo
   ]
 })
 export class AppModule { }
