9af71a6ef395887a10e288a067158219236f46c0
Blas Bravo <blas@draimo.com>
2019-09-09 09:16:38 -0300

    '

--- src/app/modulos/localidad/localidad.module.ts
+++ src/app/modulos/localidad/localidad.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { LocalidadRoutingModule } from './localidad-routing.module';
 
 import { MatTabsModule, MatAutocompleteModule, MatTableModule, MatPaginatorModule, MatSortModule, 
-  MatSelectModule, MatProgressBarModule } from '@angular/material';
+  MatSelectModule, MatProgressBarModule, MatButtonModule } from '@angular/material';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LocalidadComponent } from 'src/app/componentes/localidad/localidad.component';
 import { TextMaskModule } from 'angular2-text-mask';
 import { LocalidadService } from 'src/app/servicios/localidad.service';
+import { SubopcionPestaniaService } from 'src/app/servicios/subopcion-pestania.service';
+import { ProvinciaService } from 'src/app/servicios/provincia.service';
 
 @NgModule({
   declarations: [
@@ -26,10 +28,13 @@ import { LocalidadService } from 'src/app/servicios/localidad.service';
     MatSortModule,
     MatSelectModule,
     MatProgressBarModule,
+    MatButtonModule,
     TextMaskModule
   ],
   providers: [
-    LocalidadService
+    LocalidadService,
+    SubopcionPestaniaService,
+    ProvinciaService,
   ]
 })
 export class LocalidadModule { }
