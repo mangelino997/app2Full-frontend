7e12647f0d42e4b6f66767d1448a4bdac0ea09ef
Blas Bravo <blas@draimo.com>
2019-09-20 16:17:00 -0300

    [Cobrador][CompaniaSeguroPoliza] Correcciones en import de modulos.

--- src/app/modulos/cobrador/cobrador.module.ts
+++ src/app/modulos/cobrador/cobrador.module.ts
@@ -4,11 +4,7 @@ import { CommonModule } from '@angular/common';
 import { CobradorRoutingModule } from './cobrador-routing.module';
 
 import { MatTabsModule, MatAutocompleteModule, MatTableModule, MatPaginatorModule, MatSortModule, 
-<<<<<<< HEAD
-  MatSelectModule, MatProgressBarModule, MatDialogModule, MatButtonModule, MatIconModule } from '@angular/material';
-=======
-  MatSelectModule, MatProgressBarModule, MatDialogModule, MatButtonModule, MatDialog, MatDividerModule } from '@angular/material';
->>>>>>> b3cee1458e9c9c8a1cab36e13ac19dfddc6fd205
+  MatSelectModule, MatProgressBarModule, MatDialogModule, MatButtonModule, MatIconModule, MatDividerModule } from '@angular/material';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CobradorComponent, CambiarCobradorPrincipalDialogo } from 'src/app/componentes/cobrador/cobrador.component';
 import { CobradorService } from 'src/app/servicios/cobrador.service';
@@ -36,19 +32,15 @@ import { LoaderService } from 'src/app/servicios/loader.service';
     MatProgressBarModule,
     MatDialogModule,
     MatButtonModule,
-<<<<<<< HEAD
-    MatIconModule
-=======
-    MatDividerModule
-
->>>>>>> b3cee1458e9c9c8a1cab36e13ac19dfddc6fd205
+    MatIconModule,
+    MatDividerModule,
+    MatDialogModule
   ],
   providers: [
     CobradorService,
     SubopcionPestaniaService,
     ToastrService,
     LoaderService,
-    MatDialog,
     CobradorService,
     Cobrador
   ],
--- src/app/modulos/compania-seguro-poliza/compania-seguro-poliza.module.ts
+++ src/app/modulos/compania-seguro-poliza/compania-seguro-poliza.module.ts
@@ -1,16 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CompaniaSeguroPolizaRoutingModule } from './compania-seguro-poliza-routing.module';
-<<<<<<< HEAD
 
 import { MatTabsModule, MatAutocompleteModule, MatTableModule, MatPaginatorModule, MatSortModule, 
   MatSelectModule, MatProgressBarModule, MatButtonModule, MatDialogModule, MatIconModule } from '@angular/material';
-=======
-import {
-  MatTabsModule, MatAutocompleteModule, MatTableModule, MatPaginatorModule, MatSortModule,
-  MatSelectModule, MatProgressBarModule, MatButtonModule, MatDialogModule, MatIconModule
-} from '@angular/material';
->>>>>>> 851771be2b9f424398f1be9704ca69d2fe54b196
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CompaniaSeguroPolizaComponent } from 'src/app/componentes/compania-seguro-poliza/compania-seguro-poliza.component';
 import { CompaniaSeguroPolizaService } from 'src/app/servicios/compania-seguro-poliza.service';
@@ -24,7 +17,6 @@ import { PdfViewerModule } from 'ng2-pdf-viewer';
 @NgModule({
   declarations: [
     CompaniaSeguroPolizaComponent,
-
   ],
   imports: [
     CommonModule,
@@ -40,12 +32,8 @@ import { PdfViewerModule } from 'ng2-pdf-viewer';
     MatProgressBarModule,
     MatButtonModule,
     MatDialogModule,
-<<<<<<< HEAD
-    MatIconModule
-=======
     MatIconModule,
-    
->>>>>>> 851771be2b9f424398f1be9704ca69d2fe54b196
+    PdfViewerModule
   ],
   providers: [
     CompaniaSeguroPolizaService,
@@ -55,7 +43,6 @@ import { PdfViewerModule } from 'ng2-pdf-viewer';
     EmpresaService,
     FechaService
   ],
-  entryComponents: [
-  ]
+  entryComponents: []
 })
 export class CompaniaSeguroPolizaModule { }
