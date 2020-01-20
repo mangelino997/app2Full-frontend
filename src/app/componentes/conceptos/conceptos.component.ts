fb84415b072b61724fb10b02b5cd9394e1c899b0
Marina Basualdo <mari@draimo.com>
2020-01-20 18:55:26 -0300

    [Conceptos]Continuacion desarrollo

--- src/app/componentes/conceptos/conceptos.component.ts
+++ src/app/componentes/conceptos/conceptos.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { ConceptosService } from 'src/app/servicios/conceptos.service';
 import { TipoConceptoSueldoService } from 'src/app/servicios/tipo-concepto-sueldo.service';
 import { FormGroup, FormControl } from '@angular/forms';
+import { AfipConceptoSueldoGrupoService } from 'src/app/servicios/afip-concepto-sueldo-grupo.service';
+import { AfipConceptoSueldoService } from 'src/app/servicios/afip-concepto-sueldo.service';
+import { UnidadMedidaSueldoService } from 'src/app/servicios/unidad-medida-sueldo.service';
 
 @Component({
   selector: 'app-conceptos',
@@ -9,35 +12,80 @@ import { FormGroup, FormControl } from '@angular/forms';
   styleUrls: ['./conceptos.component.css']
 })
 export class ConceptosComponent implements OnInit {
-  public tipoConcepto: FormControl = new FormControl();
+  public tipoConceptoSueldo: FormControl = new FormControl();
+  public afipGrupoConcepto: FormControl = new FormControl();
   public codigoAfip: FormControl = new FormControl();
   public formulario: FormGroup;
-  public tiposConceptos: Array<any> = [];
-  public unidadesMedidasSueldos: Array<any> = [];
+  public itemTiposConceptosSueldos: Array<any> = [];
+  public itemGruposConceptos: Array<any> = [];
+  public itemAfipConceptosSueldo: Array<any> = [];
+  public itemUnidadesMedidasSueldos: Array<any> = [];
   public sonRepetibles : Array<any> = []
   //Define el constructor de la clase
-  constructor(private conceptosService: ConceptosService, private tipoConceptoSueldoService : TipoConceptoSueldoService) { 
+  constructor(private conceptosService: ConceptosService, private tipoConceptoSueldoService: TipoConceptoSueldoService, private afipConceptoSueldoGrupoService: AfipConceptoSueldoGrupoService, private afipConceptoSueldoService: AfipConceptoSueldoService, private unidadMedidaSueldoService: UnidadMedidaSueldoService) { 
     this.formulario = new FormGroup({
+      afipConceptoSueldo: new FormControl(),
       codigoEmpleador: new FormControl(),
+      nombre: new FormControl(),
       unidadMedidaSueldo: new FormControl(),
+      ingresaCantidad: new FormControl(),
+      ingresaValorUnitario: new FormControl(),
+      ingresaImporte: new FormControl(),
       esRepetible: new FormControl(),
+      imprimeValorUnitario: new FormControl(),
     })
   }
   //Al inicializarse el componente se ejecuta el codigo de OnInit
   ngOnInit() {
-    this.listarTipoConceptoSueldo()
+    this.listarTipoConceptoSueldo();
   }
   public listarTipoConceptoSueldo(){
     this.tipoConceptoSueldoService.listar().subscribe(
       res=>{
-        this.tiposConceptos = res.json();
-        console.log(res.json());      
+        this.itemTiposConceptosSueldos = res.json();
       },
       err=>{
         console.log('err');
       }
     )
   }
+  public listarAfipConceptoSueldoGrupo(){
+    let id = this.tipoConceptoSueldo.value.id;
+    this.afipConceptoSueldoGrupoService.listarPorTipoConceptoSueldo(id).subscribe(
+      res=>{
+        this.itemGruposConceptos = res.json();
+      },
+      err=>{
+        console.log('err');
+      }
+    )
+  }
+  public listarAfipConceptoSueldo(){
+    let id = this.afipGrupoConcepto.value.id;
+    this.afipConceptoSueldoService.listarPorAfipConceptoSueldoGrupo(id).subscribe(
+      res=>{
+        this.itemAfipConceptosSueldo = res.json();
+      },
+      err=>{
+
+      }
+    )
+  }
+  public listarUnidadMedidaSueldo(){
+    this.unidadMedidaSueldoService.listar().subscribe(
+      res=>{
+        this.itemUnidadesMedidasSueldos = res.json();
+      },
+      err=>{
+        console.log('err');
+      }
+    )
+  }
+  public seleccionarCodigoAfip(){
+    let codigo = this.formulario.get('afipConceptoSueldo').value.codigoAfip;
+    this.codigoAfip.setValue(codigo); 
+  }
+
   public liquidacion(){
   }
 
