c5787284643c306b3ba45aeae7aea48bb0a6b391
Blas Bravo <blas@draimo.com>
2020-01-21 12:58:04 -0300

    [Vehiculo][Pdf] Inicio correcciones. + [Cobranza] Inicio desarrollo.

--- src/app/componentes/pdf-dialogo/pdf-dialogo.component.ts
+++ src/app/componentes/pdf-dialogo/pdf-dialogo.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { PdfService } from 'src/app/servicios/pdf.service';
 
 @Component({
   selector: 'app-pdf-dialogo',
@@ -7,18 +8,42 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
   styleUrls: ['./pdf-dialogo.component.css']
 })
 export class PdfDialogoComponent implements OnInit {
+  //Define circulo de progreso
+  public show:boolean = false;
   //Define el nombre del pdf
   public nombre:string = null;
   //Define los datos del pdf
   public datos:string = null;
   //Constructor
-  constructor(public dialogRef: MatDialogRef<PdfDialogoComponent>, @Inject(MAT_DIALOG_DATA) public data) { }
+  constructor(public dialogRef: MatDialogRef<PdfDialogoComponent>, @Inject(MAT_DIALOG_DATA) public data,
+    private pdfService: PdfService) { }
   //Al inicializarse el componente
   ngOnInit() {
-    //Establece el nombre del pdf
-    this.nombre = this.data.nombre;
-    //Establece los datos del pdf
-    this.datos = this.data.datos;
+    //Verifica si existe el id
+    if(this.data.id) {
+      //Obtiene los datos del pdf por id
+      this.obtenerPorId(this.data.id);
+    } else {
+      this.nombre = this.data.nombre;
+      this.datos = this.data.datos;
+    }
+  }
+  //Obtiene por id
+  private obtenerPorId(id): void {
+    this.show = true;
+    this.pdfService.obtenerPorId(id).subscribe(
+      res => {
+        let respuesta = res.json();
+        if(respuesta) {
+          this.nombre = respuesta.nombre;
+          this.datos = atob(respuesta.datos);
+        }
+        this.show = false;
+      },
+      err => {
+        this.show = false;
+      }
+    );
   }
   //Descarga el pdf
   public descargar(): void {
