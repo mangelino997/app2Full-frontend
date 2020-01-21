33f698a0d59d537c5b87bbfd3a91d0b5973c4568
Marina Basualdo <mari@draimo.com>
2020-01-21 12:54:21 -0300

    [Conceptos]Continuacion del desarrollo

--- src/app/modulos/conceptos/conceptos.module.ts
+++ src/app/modulos/conceptos/conceptos.module.ts
@@ -4,9 +4,13 @@ import { CommonModule } from '@angular/common';
 import { ConceptosRoutingModule } from './conceptos-routing.module';
 import { ConceptosComponent } from 'src/app/componentes/conceptos/conceptos.component';
 import { ConceptosService } from 'src/app/servicios/conceptos.service';
-import { MatSelectModule, MatAutocompleteModule, MatButtonModule } from '@angular/material';
+import { MatSelectModule, MatAutocompleteModule, MatButtonModule, MatTabsModule, MatTableModule, MatPaginatorModule,
+   MatSortModule, MatProgressBarModule, MatIconModule, MatTooltipModule } from '@angular/material';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TipoConceptoSueldoService } from 'src/app/servicios/tipo-concepto-sueldo.service';
+import { AfipConceptoSueldoGrupoService } from 'src/app/servicios/afip-concepto-sueldo-grupo.service';
+import { AfipConceptoSueldoService } from 'src/app/servicios/afip-concepto-sueldo.service';
+import { UnidadMedidaSueldoService } from 'src/app/servicios/unidad-medida-sueldo.service';
 
 @NgModule({
   declarations: [
@@ -15,15 +19,25 @@ import { TipoConceptoSueldoService } from 'src/app/servicios/tipo-concepto-sueld
   imports: [
     CommonModule,
     ConceptosRoutingModule,
-    MatSelectModule,
+    MatTabsModule,
     FormsModule,
     ReactiveFormsModule,
     MatAutocompleteModule,
-    MatButtonModule
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatSelectModule,
+    MatProgressBarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTooltipModule
   ],
   providers: [
     ConceptosService,
-    TipoConceptoSueldoService
+    TipoConceptoSueldoService,
+    AfipConceptoSueldoGrupoService,
+    AfipConceptoSueldoService,
+    UnidadMedidaSueldoService,
   ]
 })
 export class ConceptosModule { }
